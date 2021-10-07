3a973bed39b5c2d1fe66602f70264421437cfb57
ajm262 <ajm262@txstate.edu>
2021-10-07 15:39:23 -0500

    I added a class for HITTING as well as a class for WINNING AND LOSING

---
+++ BlackJack/BlackJack/app.js
@@ -0,0 +1,15 @@
+class Player {
+    constructor() {
+
+    }
+
+    getHit(card) {
+        //this is where you get hit if you need to
+    }
+
+    outcome(game) {
+        //assuming each game with be one instance
+    }
+
+
+}
